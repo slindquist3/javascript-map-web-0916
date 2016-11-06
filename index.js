5a0cac7f2cbef6dac5a00b0a66148bcce0ee1450
slindquist3 <slindquist3@gmail.com>
2016-11-06 14:32:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+function map(collection, callback) {
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    result.push(callback(element, index, collection));
+  }
+  return result;
+}
